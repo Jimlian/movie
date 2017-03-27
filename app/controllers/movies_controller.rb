c3600b47874b8e72391fe4cc2a9ffa78d3e2f5ff
lianxu <water0301jim@163.com>
2017-03-27 19:57:00 +0800

    update all 6th part

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,14 +1,21 @@
 class MoviesController < ApplicationController
+  before_action :authenticate_user! , only: [:new, :create, :eidit, :update, :destroy]
+
   def index
     @movies = Movie.all
   end
 
   def show
     @movie = Movie.find(params[:id])
+    @posts = @movie.posts
   end
 
   def edit
    @movie = Movie.find(params[:id])
+
+   if current_user != @movie.user
+     redirect_to root_path, alert: "You have no permission."
+   end
  end
 
   def new
@@ -17,8 +24,13 @@ class MoviesController < ApplicationController
 
   def create
     @movie = Movie.new(movie_params)
-    @movie.save
+    @movie.user = current_user
+
+    if @movie.save
     redirect_to movies_path
+  else
+    render :new
+  end
   end
 
   def update
@@ -35,7 +47,7 @@ class MoviesController < ApplicationController
     flash[:alert] = " 影片已删除"
     redirect_to movies_path
   end
-  
+
 
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :movies
+  devise_for :users
+  resources :movies do
+    resources :posts
+  end
   root 'movies#index'
-
 end
